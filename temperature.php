d098e08f1d01a7166c678fc0dd603266f6663fc3
Petrius Lima <petrius.lima@gmail.com>
2016-07-23 13:58:46 -0300

    Changing from climatempo.com.br to openweathermap.org API

--- temperature.php
+++ temperature.php
@@ -1,26 +1,40 @@
 <?php
 
-$cities = [
-    'leme' => '476',
-    'rioClaro' => '531'
+$config = [
+    'baseUrl' => 'http://api.openweathermap.org/data/2.5/weather',
+    'appid' => '23a9744017bf02ed2d7ae1de269d5e93',
+    'units' => 'metric' // metric : celsius | imperial : fahrenheit | kelvin : is the default
 ];
 
-$postdata = http_build_query(
+if (empty($argv[1])) {
+    exit('The city name is required.');
+}
+
+$city = $argv[1];
+
+$country = 'br';
+if (! empty($argv[2])) {
+    $country = $argv[2];
+}
+
+$methodData = http_build_query(
     array(
-        'id' => $cities[$argv[1]]
+        'q' => $city . ',' . $country,
+        'appid' => $config['appid'],
+        'units' => $config['units']
     )
 );
 
-$opts = array('http' =>
+$requestData = array('http' =>
     array(
-        'method'  => 'POST',
-        'header'  => "Referer: http://www.climatempo.com.br/\r\nContent-type: application/x-www-form-urlencoded\r\n",
-        'content' => $postdata
+        'method'  => 'GET'
     )
 );
 
-$context  = stream_context_create($opts);
+$context  = stream_context_create($requestData);
 
-$result = file_get_contents('http://www.climatempo.com.br/json/tempo-no-momento', false, $context);
+$result = file_get_contents($config['baseUrl'] . '?' . $methodData, false, $context);
 
-echo json_decode($result)->temperature . ' graus';
+$responseVO = json_decode($result);
+
+echo 'The current temperature in ' . ucwords($city) . ' is ' . $responseVO->main->temp . 'º.' . PHP_EOL;
